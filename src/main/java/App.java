c16a1441cc5dfa500d7492a7bf76aa23c3a18216
Vladimir <vm_77@list.ru>
2023-09-20 02:05:38 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,25 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+
+        boolean a = bean == bean1;
+
+        boolean b = bean2 == bean3;
+
+        System.out.println(a);
+
+        System.out.println(b);
     }
 }
